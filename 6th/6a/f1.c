ddb16f86df09876273191341e224528cf52452e1
Kartik Hegadi <67619115+kartikhegadi@users.noreply.github.com>
2023-07-18 16:30:50 +0530

    my programs
    commit 2

---
+++ 6th/6a/f1.c
@@ -0,0 +1,7 @@
+#include<stido.h>
+int main()
+{
+ // this is a comment
+ printf("hello");
+ /* this is another comment */
+}
---
+++ 6th/6b/y.tab.h
@@ -0,0 +1,9 @@
+#ifndef _yy_defines_h_
+#define _yy_defines_h_
+
+#define DIGIT 257
+#define ID 258
+#define KEY 259
+#define OP 260
+
+#endif /* _yy_defines_h_ */
